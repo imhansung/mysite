1863017a611977792d421aaf859637e955f1ac45
LAPTOP-TPFDEV-G\tpf-dev <dev@tpf.or.kr>
2025-09-30 17:10:34 +0900

    2-10 폼 -1 폼 html 자동작성

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm
 
 
 def index(request):
@@ -17,3 +18,17 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():  # 폼이 유효하다면
+            question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴받는다.
+            question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정한다.
+            question.save()  # 데이터를 실제로 저장한다.
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+
+    return render(request, 'pybo/question_form.html', context)
