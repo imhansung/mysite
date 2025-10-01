a4647e8b35bc4c7f85a9cc2041614dfdc8785fbc
LAPTOP-TPFDEV-G\tpf-dev <dev@tpf.or.kr>
2025-10-01 11:17:40 +0900

    3-07 모델 변경

--- pybo/models.py
+++ pybo/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.subject
@@ -14,3 +15,4 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
