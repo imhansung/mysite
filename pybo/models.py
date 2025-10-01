f797dac06d71278e8b9146403b5fc309431cb0af
LAPTOP-TPFDEV-G\tpf-dev <dev@tpf.or.kr>
2025-10-01 17:32:04 +0900

    3-11 추천

--- pybo/models.py
+++ pybo/models.py
@@ -6,7 +6,8 @@ class Question(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
     
     def __str__(self):
         return self.subject
@@ -17,4 +18,5 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    voter = models.ManyToManyField(User, related_name='voter_answer')
